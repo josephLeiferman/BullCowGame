dde1e15ede5420eb4cf99407a0dca45e1a92da63
josephLeiferman <joseph.leiferman@gmail.com>
2020-07-03 21:22:24 -0500

    Add more complexity to game loop

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,22 +4,41 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    // Setting up game
+    SetupGame();
+    
+    //PrintLine(TEXT("The HiddenWord is %s."), *HiddenWord)); // Debug Line
+
     PrintLine(TEXT("Welcome to Bull Cows!"));
-    PrintLine(TEXT("Guess the 3 letter word!")); // Magic Number please remove
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len()); // Magic Number please remove
     PrintLine(TEXT("Press enter to continue..."));
 
+    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("pug"); // Make this an instance variable instead
+    PrintLine(TEXT("You have %i lives left"), Lives);
+    
     if (HiddenWord == Input) 
     {
         PrintLine(TEXT("You have won!"));
     } 
     else 
     {
+        if (HiddenWord.Len() != Input.Len()) 
+        {
+            PrintLine(TEXT("The Hidden word is %i characters long, try again"), HiddenWord.Len());
+        }
         PrintLine(TEXT("You have lost!"));
+        Lives--;
     }
+}
+
+void UBullCowCartridge::SetupGame() 
+{
+    HiddenWord = TEXT("pugs");
+    Lives = HiddenWord.Len();
 }
